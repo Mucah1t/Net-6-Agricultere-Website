879d6b9c730f730dcbdd8c96e68fc50e58ef416a
mtyilmaz58@outlook.com <mtyilmaz58@outlook.com>
2023-01-02 00:10:58 +0300

    Some additional tables are added by codefirst approach to make website more dynamic and ViewComponents are used.

---
+++ AgriculturePresentation/ViewComponents/_TopAddressPartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.ViewComponents
+{
+	public class _TopAddressPartial : ViewComponent 
+	{
+		private readonly IAdressService _addressService;
+
+		public _TopAddressPartial(IAdressService addressService)
+		{
+			_addressService = addressService;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var vList = _addressService.GetListAll();
+			return View(vList);
+		}
+	}
+}
--- DataAccessLayer/Migrations/20230101195638_mig_add-SocialMedia.Designer.cs
+++ DataAccessLayer/Migrations/20230101195638_mig_add-SocialMedia.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(AgricultureContext))]
-    partial class AgricultureContextModelSnapshot : ModelSnapshot
+    [Migration("20230101195638_mig_add-SocialMedia")]
+    partial class mig_addSocialMedia
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -21,27 +23,6 @@ namespace DataAccessLayer.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
-
-            modelBuilder.Entity("EntityLayer.Concrete.About", b =>
-                {
-                    b.Property<int>("AboutID")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("AboutID"), 1L, 1);
-
-                    b.Property<string>("AboutHistory")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("AboutUs")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("AboutID");
-
-                    b.ToTable("Abouts");
-                });
 
             modelBuilder.Entity("EntityLayer.Concrete.Adress", b =>
                 {
