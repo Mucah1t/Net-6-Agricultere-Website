32bd7fa50f3f9807935f0d836d0f4a5edb5256ca
mtyilmaz58@outlook.com <mtyilmaz58@outlook.com>
2023-01-03 01:59:34 +0300

    ViewComponents are done.

---
+++ AgriculturePresentation/ViewComponents/_AddressPartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.ViewComponents
+{
+	public class _AddressPartial:ViewComponent
+	{
+		private readonly IAdressService _addressService;
+
+		public _AddressPartial(IAdressService addressService)
+		{
+			_addressService = addressService;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var vList= _addressService.GetListAll();
+			return View(vList);
+		}
+	}
+}
